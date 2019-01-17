d0c48e9d0e3e0ac15774a0fb887809e6508c2ccb
s315614 <s315614@stud.hioa.no>
2019-01-17 05:42:55 +0100

    ..

--- counting.java
+++ counting.java
@@ -1,16 +1,27 @@
 public class counting{
+	static String[] statsjon = {"parking", "parking2"};
 	static int[] parking = {0,0,1,1,0,0,1,1,0,1,1,1,0,0,1,1,0,0,1,1}; // 0 er ledige plass og 1 er ledige sykkeler
+	static int statsjoner = 0;
 	static int ledigPlass = 0;
 	static int ledigSykkel = 0;
+	static String output;
 	public static void main(String[] args) {
-		for (int i = 0; i<parking.length; i++) {
-			if (parking[i] == 0) {
-				ledigPlass ++;
-			} else if (parking[i] == 1) {
-				ledigSykkel ++;
-			}
+		for (int j = 0; j< statsjon.length; j++ ) {
+			if (statsjon[j] == "parking") {
+				for (int i = 0; i<parking.length; i++) {
+					if (parking[i] == 0) {
+						ledigPlass ++;
+					} else if (parking[i] == 1) {
+						ledigSykkel ++;
+					}
+					output = statsjon[j] + " har " + ledigPlass + " ledige plass og " + ledigSykkel+ " ledige sykkler.";
 
+				}
+			}
+			
+			statsjoner++;
 		}
-		System.out.println("Ledige plass er: " + ledigPlass + " og ledige Sykkeler er: " + ledigSykkel);
+		
+		System.out.println("Vi har "+statsjoner+"statsjoner og " + output);
 	}
 }
